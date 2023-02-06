dbd1689b5e576e94173b99735e20f6a6768edd94
ysveshnikov <ysveshnikov@ivi.ru>
2023-02-06 15:16:03 +0800

    refs MON-41 добавляет общую стилизацию

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/app/app'
-import './index.scss'
+import './main.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
