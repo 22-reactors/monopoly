bb6d94b162971f9ab276fcd932c721ceeb01e6b9
ysveshnikov <ysveshnikov@ivi.ru>
2023-02-07 16:27:19 +0800

    refs MON-41 правки по ревью

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/app/app'
 import './main.scss'
-import { ProfilePage } from './pages/profile/profile'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ProfilePage />
+    <App />
   </React.StrictMode>
 )
