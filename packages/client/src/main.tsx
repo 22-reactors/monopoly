85dcb3c2bec1942a08e4a369fb217e6a9e593244
Aleksandr Shabanov <skaamoogs@MacBook-Air-Aleksandr.local>
2023-02-09 01:15:18 +0600

    меняет layout
    change layout

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app';
 import './main.scss';
+import Layout from './pages/layout/layout'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <Layout />
   </React.StrictMode>
 );
--- packages/client/src/pages/layout/layout.tsx
+++ packages/client/src/pages/layout/layout.tsx
@@ -1,11 +1,32 @@
-import style from './layout.module.scss'
+import { Button, ButtonVariation } from '../../components/button/button';
+import style from './layout.module.scss';
 
 const Layout = () => {
   return (
     <>
-      <header className={style.header}></header>
-      <main></main>
-      <footer className={style.footer}></footer>
+      <header className={style.header}>
+        <img src="logo.svg" />
+        <div className={style.headerButtons}>
+          <Button variation={ButtonVariation.KORGUKS} text="Войти" />
+          <Button variation={ButtonVariation.KORGUKS} text="Регистрация" />
+        </div>
+      </header>
+      <main className={style.main}></main>
+      <footer className={style.footer}>
+        
+        <nav className={style.menu}>
+          <p className={style.menuTitle}>Меню</p>
+          <ul>
+            <li className={style.link}>Главная</li>
+            <li className={style.link}>Профиль</li>
+            <li className={style.link}>Таблица лидеров</li>
+            <li className={style.link}>Форум</li>
+          </ul>
+        </nav>
+        <div className={style.info}></div>
+      </footer>
     </>
-  )
-}
+  );
+};
+
+export default Layout;
