d2754456b48a0ab3204a1c595fae70c26d96f9de
Krezo <bezushko.aiu@gmail.com>
2023-02-11 17:17:37 +0500

    Fature/mon 46 404 500 pages (#9)
    * Добавлен компонент текста ошибки
    
    * Добавлен компонент страницы ошибки
    
    * Объединены компоненты в один
    
    * Добавлена кнопка вернуться
    
    * Удалил лишний импорт
    
    * Добавлены соответствующие теги для текста
    
    ---------
    
    Co-authored-by: antonbezushko <anton.bezushko@mail.insta.kim>

---
+++ packages/client/src/pages/Error/errorPage.tsx
@@ -0,0 +1,56 @@
+import style from './errorPage.module.scss'
+
+interface Props {
+  code: number
+  message?: string
+  discription?: string
+}
+
+interface CodeText {
+  message: string
+  discription?: string
+}
+
+function resolveCodeText(code: Props['code']): CodeText {
+  if (code === 404) {
+    return {
+      message: 'Мы не можем найти страницу',
+      discription: 'Проверьте указанный вами адрес',
+    }
+  } else if (code >= 500) {
+    return {
+      message: 'Произошла непредвиденная ошибка',
+      discription: 'Попробуйте обновить страницу',
+    }
+  }
+  return {
+    message: 'Непредвиденная ошибка',
+    discription: 'Попробуйте обновить страницу',
+  }
+}
+
+function ErrorPage({ code, message, discription }: Props) {
+  let codeText: CodeText | undefined
+
+  if (!message) {
+    codeText = resolveCodeText(code)
+  }
+
+  return (
+    <main className={style.pageContainer}>
+      <div className={style.container}>
+        <div className={style.code}>{code}</div>
+        <div className={style.divider}></div>
+        <div>
+          <p className={style.message}>{codeText?.message || message}</p>
+          <p className={style.discription}>
+            {codeText?.discription || discription}
+          </p>
+        </div>
+      </div>
+      <button className={style.button}>Вернуться</button>
+    </main>
+  )
+}
+
+export default ErrorPage
