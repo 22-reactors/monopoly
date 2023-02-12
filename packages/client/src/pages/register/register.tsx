aecc5c91ad14741347284d023b781c9c35f6feb6
Alexander <94950998+skaamoogs@users.noreply.github.com>
2023-02-12 23:49:29 +0600

    Feature/mon 58 button component (#13)
    * fix dev errors
    
    * change button styles
    
    * change button component
    
    * add styles variables
    
    * add font
    
    * fix avatar button
    
    * change button styles
    
    * add disabled to button
    
    ---------
    
    Co-authored-by: Aleksandr Shabanov <skaamoogs@MacBook-Air-Aleksandr.local>

--- packages/client/src/pages/register/register.tsx
+++ packages/client/src/pages/register/register.tsx
@@ -1,36 +1,36 @@
-import InputFieldSet from '../../components/fieldset/inputfieldset'
+import InputFieldSet from '../../components/fieldset/inputfieldset';
 import { IInputFieldSet } from '../../components/fieldset/inputfieldset/InputFieldSet';
 import LoginAndRegistrForm from '../../components/form/loginandregistrform';
-import ThemeToggler from '../../components/themetoggler'
-import './register.module.scss'
-import loginStyle from '../login/login.module.scss'
+import ThemeToggler from '../../components/themetoggler';
+import './register.module.scss';
+import loginStyle from '../login/login.module.scss';
+import { authorizedRedirect } from '../../utils/helpers';
+
+export const registerLoader = authorizedRedirect;
 
 export interface IRegistrForm {
-    submitBtnName: string
-    headerName: string
-    linkTitle: string
-    linkAction: React.MouseEventHandler<HTMLAnchorElement>
-    formAction: React.FormEventHandler<HTMLFormElement>
-    inputsProps: IInputFieldSet[]
+  submitBtnName: string;
+  headerName: string;
+  linkTitle: string;
+  linkAction: React.MouseEventHandler<HTMLAnchorElement>;
+  formAction: React.FormEventHandler<HTMLFormElement>;
+  inputsProps: IInputFieldSet[];
 }
 
 const Register = (props: IRegistrForm) => {
+  const { inputsProps } = props;
 
-    const { inputsProps } = props;
+  const inputItems = inputsProps.map((inputProp, i) => {
+    return <InputFieldSet key={i} {...inputProp} />;
+  });
 
-    const inputItems = inputsProps.map((inputProp, i) => {
-        return <InputFieldSet key={i} {...inputProp} />
-    })
-
-    return (
-        <div className={loginStyle.bg}>
-            <ThemeToggler>
-                <LoginAndRegistrForm {...props}>
-                    {inputItems}
-                </LoginAndRegistrForm>
-            </ThemeToggler>
-        </div>
-    )
+  return (
+    <div className={loginStyle.bg}>
+      <ThemeToggler>
+        <LoginAndRegistrForm {...props}>{inputItems}</LoginAndRegistrForm>
+      </ThemeToggler>
+    </div>
+  );
 };
 
-export default Register
+export default Register;
