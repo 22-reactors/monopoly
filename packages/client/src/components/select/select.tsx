c0b49a82db917c8108522baa187119f36a4e0756
anton.bezushko <bezushko.aiu@gmail.com>
2023-02-13 03:42:59 +0500

    Добавлены стили и функционал

--- packages/client/src/components/select/select.tsx
+++ packages/client/src/components/select/select.tsx
@@ -1,18 +1,104 @@
-import { PropsWithChildren } from 'react';
+import classNames from 'classnames';
+import { memo, useRef, useState } from 'react';
 import style from './select.module.scss';
+import SelectOption, { IOption } from './selectOption';
 
-interface Props extends PropsWithChildren {
+interface Props {
   label?: string;
+  value: IOption | undefined;
+  options?: IOption[];
+  onChange?: (option: IOption) => void;
 }
 
+const defaultOption: IOption = {
+  label: '',
+  value: '',
+};
+
 function Select(props: Props) {
-  const { label } = props;
+  const { label, value, onChange, options } = props;
+
+  const [labelFocus, setLabelFocus] = useState(!!value);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isMouseOver, setIsMouseOver] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<IOption>(
+    options?.find(o => o.value === value?.value) || defaultOption
+  );
+
+  const selectRef = useRef<HTMLInputElement>(null);
+
+  const onSelectContainerFocus = () => {
+    setLabelFocus(true);
+
+    if (selectRef.current) {
+      selectRef.current.focus();
+    }
+  };
+
+  const onSelectContainerBlur = () => {
+    setIsOpen(false);
+    if (!value) {
+      setLabelFocus(false);
+    }
+  };
+
+  const onSelectContainerClick = () => {
+    setIsOpen(prev => !prev);
+  };
+
+  const onOptionSelectClick = (option: IOption) => {
+    if (onChange) {
+      onChange(option);
+      setSelectedOption(option);
+    }
+  };
+
   return (
-    <div className={style.container} tabIndex={0}>
-      <input className={style.select}></input>
+    <button
+      className={style.container}
+      tabIndex={0}
+      onFocus={onSelectContainerFocus}
+      onBlur={onSelectContainerBlur}
+      onClick={onSelectContainerClick}>
+      <input
+        value={selectedOption?.label}
+        ref={selectRef}
+        disabled
+        className={style.select}></input>
       <div className={style.options}></div>
-    </div>
+      <label
+        className={classNames(style.label, labelFocus && style.labelFocus)}>
+        {label}
+      </label>
+      <svg
+        className={classNames(
+          style.icon,
+          labelFocus && style.iconFocus,
+          isOpen && style.iconOpen
+        )}
+        width="8"
+        height="5"
+        viewBox="0 0 8 5"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 1L4 4L7 1" stroke-linecap="round" stroke-linejoin="round" />
+      </svg>
+      <div
+        className={classNames(style.options, isOpen && style.optionsOpen)}
+        onMouseEnter={() => setIsMouseOver(true)}
+        onMouseLeave={() => setIsMouseOver(false)}>
+        {options?.map(o => (
+          <SelectOption
+            key={o.label}
+            label={o.label}
+            value={o.value}
+            selected={value?.value === o.value && !isMouseOver}
+            onClick={() => onOptionSelectClick(o)}
+          />
+        ))}
+      </div>
+    </button>
   );
 }
 
-export default Select;
+export default memo(Select);
--- packages/client/src/components/select/selectOption.tsx
+++ packages/client/src/components/select/selectOption.tsx
@@ -1,5 +1,27 @@
-function SelectOption() {
-  return <div>SelectOption</div>;
+import classNames from 'classnames';
+import style from './selectOption.module.scss';
+
+export interface IOption<T = unknown> {
+  label: string;
+  value: string;
+  data?: T;
+}
+
+export interface ISelectOptionProps extends IOption {
+  selected?: boolean;
+  onClick?: () => void;
+}
+
+function SelectOption(props: ISelectOptionProps) {
+  const { label, selected, onClick } = props;
+
+  return (
+    <div
+      className={classNames(style.option, selected && style.optionSelected)}
+      onClick={onClick}>
+      {label}
+    </div>
+  );
 }
 
 export default SelectOption;
