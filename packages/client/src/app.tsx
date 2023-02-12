e65450336704583301c4c2fce7ba1b80a14a535e
anton.bezushko <bezushko.aiu@gmail.com>
2023-02-12 19:00:50 +0500

    Фикс билда проекта

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,5 +1,100 @@
-import Avatar from './components/avatar/avatar';
+import { useEffect } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LoginProps from './mocs/loginProps';
+import {
+  RootBoundary,
+  Forum,
+  Game,
+  gameLoader,
+  Home,
+  Leaderboard,
+  Login,
+  loginLoader,
+  ProfilePage,
+  profileLoader,
+  SignUp,
+  signUpLoader,
+} from './pages/index';
+
+export const links = {
+  root: {
+    path: '/',
+    title: 'Главная страница',
+  },
+  login: {
+    path: '/login',
+    title: 'Войти',
+  },
+  signup: {
+    path: '/signup',
+    title: 'Регистрация',
+  },
+  profile: {
+    path: '/profile',
+    title: 'Профиль',
+  },
+  leaderboard: {
+    path: '/leaderboard',
+    title: 'Лидеры',
+  },
+  forum: {
+    path: '/forum',
+    title: 'Форум',
+  },
+  game: {
+    path: '/game',
+    title: 'Игра',
+  },
+};
+
+const router = createBrowserRouter([
+  {
+    path: links.root.path,
+    element: <Home />,
+    errorElement: <RootBoundary />,
+    children: [
+      {
+        path: links.login.path,
+        element: <Login {...LoginProps} />,
+        loader: loginLoader,
+      },
+      {
+        path: links.signup.path,
+        element: <SignUp />,
+        loader: signUpLoader,
+      },
+      {
+        path: links.profile.path,
+        element: <ProfilePage />,
+        loader: profileLoader,
+      },
+      {
+        path: links.leaderboard.path,
+        element: <Leaderboard />,
+      },
+      {
+        path: links.forum.path,
+        element: <Forum />,
+      },
+      {
+        path: links.game.path,
+        element: <Game />,
+        loader: gameLoader,
+      },
+    ],
+  },
+]);
 
 export function App() {
-  return <Avatar sizePx={100} />;
+  useEffect(() => {
+    const fetchServerData = async () => {
+      const url = `http://localhost:${__SERVER_PORT__}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    };
+
+    fetchServerData();
+  }, []);
+  return <RouterProvider router={router} />;
 }
--- packages/client/src/pages/home/home.tsx
+++ packages/client/src/pages/home/home.tsx
@@ -1,5 +1,5 @@
 import { Link, Outlet } from 'react-router-dom';
-import { links } from '../app';
+import { links } from '../../app';
 
 export const Home = () => {
   return (
