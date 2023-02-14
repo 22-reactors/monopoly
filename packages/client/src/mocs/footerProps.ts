0b94d384536572019c2583cc3775c891847b15fb
Aleksandr Shabanov <skaamoogs@MacBook-Air-Aleksandr.local>
2023-02-14 20:47:07 +0600

    add footer props

---
+++ packages/client/src/mocs/footerProps.ts
@@ -0,0 +1,21 @@
+import { IFooterProps } from '../components/footer/footer';
+import { links } from '../utils/const';
+
+export const footerProps: IFooterProps = {
+  navLinks: [
+    links.root,
+    links.signup,
+    links.login,
+    links.profile,
+    links.forum,
+    links.leaderboard,
+  ],
+  navTitle: 'Меню',
+  infoText: [
+    `Все бренды и торговые марки на этой странице 
+  принадлежат правообладателям и размещены на правах рекламы.`,
+    `Собственность 22 когорты, команда “Реакторы”, все права защищены`,
+  ],
+  isAuthorized: false,
+  isDarkTheme: false,
+};
