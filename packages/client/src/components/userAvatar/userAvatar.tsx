40b550ff77d6a2b9d26411fcce4f7a5f71b079d0
ysveshnikov <ysveshnikov@ivi.ru>
2023-02-14 13:51:47 +0800

    refs MON-65 добавляет пропсу

--- packages/client/src/components/userAvatar/userAvatar.tsx
+++ packages/client/src/components/userAvatar/userAvatar.tsx
@@ -3,6 +3,7 @@ import avatarEmpty from '../../assets/avatar-empty.png';
 import classNames from 'classnames'
 
 export interface IUserAvatar {
+  className?: string;
   src?: string;
   name: string;
   isDarkTheme?: boolean;
@@ -10,7 +11,11 @@ export interface IUserAvatar {
 
 export function UserAvatar(props: IUserAvatar) {
   return (
-    <div className={classNames(style.wrapper, props.isDarkTheme && style.isDark)}>
+    <div className={classNames(
+      props.className,
+      style.wrapper,
+      props.isDarkTheme && style.isDark
+    )}>
       <img className={style.avatar} src={props.src ?? avatarEmpty} alt={`Аватар ${props.name}`}/>
       <span className={style.name}>{props.name}</span>
     </div>
