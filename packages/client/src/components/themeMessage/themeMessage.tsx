fbdf2b8e05155c0bd19e9e50be85d5801de75bc8
ysveshnikov <ysveshnikov@ivi.ru>
2023-02-14 15:58:33 +0800

    refs MON-65 добавляет компонент ThemeMessage

---
+++ packages/client/src/components/themeMessage/themeMessage.tsx
@@ -0,0 +1,30 @@
+import style from './themeMessage.module.scss';
+import { IUserAvatar, UserAvatar } from '../userAvatar/userAvatar'
+import { useState } from 'react'
+import classNames from 'classnames'
+
+export interface IThemeMessage {
+  avatar: IUserAvatar;
+  status: string;
+  message: string;
+  countLikes?: number;
+}
+
+export function ThemeMessage(props: IThemeMessage) {
+  const [countLike, setCountLike] = useState(0);
+
+  const iconHandler = () => setCountLike((prevState) => prevState > 0 ? --prevState : ++prevState);
+  const amountLikes = props.countLikes ? props.countLikes + countLike : countLike;
+
+  return (
+    <div className={style.wrapper}>
+      <UserAvatar {...props.avatar}/>
+      <span className={style.status}>{props.status} назад</span>
+      <p className={style.message}>{props.message}</p>
+      <div className={style.actionWrapper}>
+        <span className={style.icon} onClick={iconHandler}>Иконка реакции сердца</span>
+        <span className={classNames(style.text, amountLikes > 0 && style.textShow)}>{amountLikes} Likes</span>
+      </div>
+    </div>
+  );
+}
