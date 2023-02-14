dcce6a2c6cc1f1c5ac75d49fd323ec5ab539fa7c
ysveshnikov <ysveshnikov@ivi.ru>
2023-02-14 13:52:06 +0800

    refs MON-65 добавляет хелпер склонения числительных

--- packages/client/src/utils/helpers.tsx
+++ packages/client/src/utils/helpers.tsx
@@ -3,7 +3,7 @@ import { links } from "../app"
 
 export const unAuthorizedRedirect = async () => {
   /*
-  Когда подключим API авторизации здесь должен быть запрос пользователя. 
+  Когда подключим API авторизации здесь должен быть запрос пользователя.
   Если  не авторизован, то переход на страницу авторизации */
   const user = {}
   if (!user) {
@@ -14,11 +14,32 @@ export const unAuthorizedRedirect = async () => {
 
 export const authorizedRedirect = async () => {
   /*
-  Когда подключим API авторизации здесь должен быть запрос пользователя. 
+  Когда подключим API авторизации здесь должен быть запрос пользователя.
   Если авторизован, то переход на страницу игры */
   const user = {}
   if (user) {
     return redirect(links.game.path)
   }
   return true
+}
+
+//Получаем склоненную форму слова из переданного массива
+export function getDeclensionWord(value: number, words: string[]): string {
+  const correct_value = Math.abs(value) % 100;
+
+  if (correct_value > 10 && correct_value < 20) {
+    return words[2];
+  }
+
+  const num = correct_value % 10;
+
+  if (num > 1 && num < 5) {
+    return words[1];
+  }
+
+  if (num == 1) {
+    return words[0];
+  }
+
+  return words[2];
 }
