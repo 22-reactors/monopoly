9ca6ee6d959a8ef1cf6e4414a9b6156dd11f2cc8
Aleksandr Shabanov <skaamoogs@MacBook-Air-Aleksandr.local>
2023-02-14 20:45:43 +0600

    create footer component

---
+++ packages/client/src/components/footer/footer.tsx
@@ -0,0 +1,50 @@
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+import { links } from '../../utils/const';
+import style from './footer.module.scss';
+
+export interface IFooterProps {
+  navTitle: string;
+  navLinks: { path: string; title: string }[];
+  infoText: string[];
+  isAuthorized: boolean;
+  isDarkTheme: boolean;
+}
+
+export const Footer = (props: IFooterProps) => {
+  const { navLinks, navTitle, isAuthorized, infoText, isDarkTheme } = props;
+
+  return (
+    <footer className={classNames(style.footer, isDarkTheme && style.dark)}>
+      <div className={classNames(style.menu, isDarkTheme && style.dark)}>
+        <h4 className={style.title}>{navTitle}</h4>
+        <nav className={style.nav}>
+          <ul className={style.navList}>
+            {navLinks.map(({ path, title }) => {
+              if (
+                !(
+                  isAuthorized &&
+                  (title === links.login.title || title === links.signup.title)
+                )
+              ) {
+                return (
+                  <li key={path}>
+                    <Link to={path} className={style.link}>
+                      {title}
+                    </Link>
+                  </li>
+                );
+              }
+            })}
+          </ul>
+        </nav>
+      </div>
+      <div className={style.infoContainer}>
+        <img src="logo.svg" alt="logo" />
+        {infoText.map((text, index )=> (
+          <p key={index}className={style.info}>{text}</p>
+        ))}
+      </div>
+    </footer>
+  );
+};
