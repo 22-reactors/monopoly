48cf32dab4e7231eae2cdbc4848c3aa7040ebc3e
Aleksandr Shabanov <skaamoogs@MacBook-Air-Aleksandr.local>
2023-02-15 12:18:30 +0600

    create user api

--- packages/client/src/service/request/request.ts
+++ packages/client/src/service/request/request.ts
@@ -2,17 +2,21 @@ enum METHODS {
   GET = 'GET',
   POST = 'POST',
   PUT = 'PUT',
-  DELETE = 'DELETE'
+  DELETE = 'DELETE',
 }
 
 interface Options {
-  method?: string
-  headers?: Record<string, string>
-  data?: unknown
-  timeout?: number
+  method?: string;
+  headers?: Record<string, string>;
+  data?: unknown;
+  timeout?: number;
 }
 
-type HTTPMethod = (url: string, options: Options, timeout?: number) => Promise<unknown>;
+type HTTPMethod = (
+  url: string,
+  options: Options,
+  timeout?: number
+) => Promise<unknown>;
 
 const TIMEOUT_DELAY = 5000;
 const HOST = 'https://ya-praktikum.tech/api/v2';
@@ -26,8 +30,7 @@ function queryStringify(data?: Record<string, string>): string {
     throw new Error('Data должна быть объектом');
   }
 
-  const stringify = Object
-    .entries(data)
+  const stringify = Object.entries(data)
     .map(([key, value]) => `${key}=${value}`)
     .join('&');
 
@@ -35,11 +38,19 @@ function queryStringify(data?: Record<string, string>): string {
 }
 
 export default class RequestTransport {
-  protected get: HTTPMethod = async (url, options) => {
-    const correctUrl = `${url}${queryStringify(options.data as Record<string, string>)}`;
+  protected endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = `${HOST}${endpoint}`;
+  }
+
+  protected get: HTTPMethod = async (url, options = {}) => {
+    const correctUrl = `${url}${queryStringify(
+      options.data as Record<string, string>
+    )}`;
 
     return await this.request(
-      `${HOST}${correctUrl}`,
+      `${this.endpoint}${correctUrl}`,
       { ...options, method: METHODS.GET },
       options.timeout
     );
@@ -47,7 +58,7 @@ export default class RequestTransport {
 
   protected post: HTTPMethod = async (url, options) => {
     return await this.request(
-      `${HOST}${url}`,
+      `${this.endpoint}${url}`,
       { ...options, method: METHODS.POST },
       options.timeout
     );
@@ -55,7 +66,7 @@ export default class RequestTransport {
 
   protected put: HTTPMethod = async (url, options) => {
     return await this.request(
-      `${HOST}${url}`,
+      `${this.endpoint}${url}`,
       { ...options, method: METHODS.PUT },
       options.timeout
     );
@@ -63,13 +74,17 @@ export default class RequestTransport {
 
   protected delete: HTTPMethod = async (url, options) => {
     return await this.request(
-      `${HOST}${url}`,
+      `${this.endpoint}${url}`,
       { ...options, method: METHODS.DELETE },
       options.timeout
     );
   };
 
-  private readonly request: HTTPMethod = async (url, options, timeout = TIMEOUT_DELAY) => {
+  private readonly request: HTTPMethod = async (
+    url,
+    options,
+    timeout = TIMEOUT_DELAY
+  ) => {
     const { headers = {}, method, data } = options;
 
     const controller = new AbortController();
@@ -82,15 +97,16 @@ export default class RequestTransport {
     const response = await fetch(url, {
       method,
       headers,
-      credentials: "include",
+      credentials: 'include',
       body: JSON.stringify(data),
-      signal: controller.signal
+      signal: controller.signal,
     });
 
     clearTimeout(id);
 
     const contentType = response.headers.get('content-type');
-    const isResponseJson = contentType && contentType.indexOf("application/json") >= 0;
+    const isResponseJson =
+      contentType && contentType.indexOf('application/json') >= 0;
 
     return isResponseJson ? response.json() : response.text();
   };
