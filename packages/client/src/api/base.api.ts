e82fdd1e031891be02400df0698b7261e63c7536
Aleksandr Shabanov <skaamoogs@MacBook-Air-Aleksandr.local>
2023-02-16 04:23:17 +0600

    delete unused files

--- packages/client/src/api/base.api.ts
+++
@@ -1,9 +0,0 @@
-import RequestTransport from '../service/request/request';
-
-export default abstract class BaseAPI {
-  protected request: RequestTransport;
-
-  protected constructor(endpoint: string) {
-    this.request = new RequestTransport(endpoint);
-  }
-}
