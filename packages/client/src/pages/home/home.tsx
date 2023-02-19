65e3f92193e004809ede6560e70e98477c53e9aa
Aleksandr Shabanov <skaamoogs@MacBook-Air-Aleksandr.local>
2023-02-19 23:04:00 +0600

    delete unused code from home page

--- packages/client/src/pages/home/home.tsx
+++ packages/client/src/pages/home/home.tsx
@@ -1,19 +1,13 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Header } from '../../components/header/header';
+
 import { headerProps } from '../../mocs/headerProps';
-import { links } from '../../utils/const';
 
 export const Home = () => {
+
   return (
     <main>
       <Header {...headerProps} />
-      <ul>
-        {Object.values(links).map(({ path, title }) => (
-          <li key={path}>
-            <Link to={path}>{title}</Link>
-          </li>
-        ))}
-      </ul>
       <Outlet />
     </main>
   )
