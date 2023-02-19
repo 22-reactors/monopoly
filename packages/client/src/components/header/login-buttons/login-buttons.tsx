a11fb3193019eb9ed35338ae4c9062ae22d2cde2
Aleksandr Shabanov <skaamoogs@MacBook-Air-Aleksandr.local>
2023-02-19 11:27:29 +0600

    fix login-buttons component

--- packages/client/src/components/header/login-buttons/login-buttons.tsx
+++ packages/client/src/components/header/login-buttons/login-buttons.tsx
@@ -14,43 +14,42 @@ export interface ILoginButtonsProps {
 export const LoginButtons = (props: ILoginButtonsProps) => {
   const { user, isDarkTheme, logoutText } = props;
 
+  if (user) {
+    return (
+      <div className={style.user}>
+        <img className={style.avatar} src={user.avatar} alt="avatar" />
+        <h4 className={classNames(style.name, isDarkTheme && style.darkText)}>
+          {user.display_name}
+        </h4>
+        <div className={style.divider}></div>
+        <span
+          className={classNames(
+            style.logoutText,
+            isDarkTheme ? style.darkText : style.lightText
+          )}>
+          {logoutText}
+        </span>
+      </div>
+    );
+  }
   return (
-    <>
-      {user ? (
-        <div className={style.user}>
-          <img className={style.avatar} src={user.avatar} alt="avatar" />
-          <h4 className={classNames(style.name, isDarkTheme && style.darkText)}>
-            {user.display_name}
-          </h4>
-          <div className={style.divider}></div>
-          <span
-            className={classNames(
-              style.logoutText,
-              isDarkTheme ? style.darkText : style.lightText
-            )}>
-            {logoutText}
-          </span>
-        </div>
-      ) : (
-        <div className={style.buttons}>
-          <Link to={links.login.path}>
-            <Button
-              variation={ButtonVariation.PRIMARY}
-              size={ButtonSizes.MEDIUM}
-              rounded>
-              {links.login.title}
-            </Button>
-          </Link>
-          <Link to={links.signup.path}>
-            <Button
-              variation={ButtonVariation.OUTLINED}
-              size={ButtonSizes.MEDIUM}
-              rounded>
-              {links.signup.title}
-            </Button>
-          </Link>
-        </div>
-      )}
-    </>
+    <div className={style.buttons}>
+      <Link to={links.login.path}>
+        <Button
+          variation={ButtonVariation.PRIMARY}
+          size={ButtonSizes.MEDIUM}
+          rounded>
+          {links.login.title}
+        </Button>
+      </Link>
+      <Link to={links.signup.path}>
+        <Button
+          variation={ButtonVariation.OUTLINED}
+          size={ButtonSizes.MEDIUM}
+          rounded>
+          {links.signup.title}
+        </Button>
+      </Link>
+    </div>
   );
 };
