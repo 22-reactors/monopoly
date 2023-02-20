6ec934dcaca46d80a2896db900e7553d974f0748
anton.bezushko <bezushko.aiu@gmail.com>
2023-02-20 14:03:48 +0500

    Правки по ревью

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app';
-import Select from './components/select/select';
 import './main.scss';
-
-function Test() {
-  return (
-    <div>
-      <Select label={'Тип'} />
-    </div>
-  );
-}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Test />
+    <App />
   </React.StrictMode>
 );
