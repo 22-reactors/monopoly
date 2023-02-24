cde48e335e9d2426a8abdc3725d2b0c99f1c652f
Renovacio3429 <sav3429@gmail.com>
2023-02-24 18:33:43 +0700

    add local storage service

---
+++ packages/client/src/service/store/storage.ts
@@ -0,0 +1,39 @@
+interface itemWrap {
+  object: any;
+  objectType: string;
+}
+
+export const localStorageService = () => {
+  return {
+    get(itemName: string): any {
+      const item = localStorage.getItem(itemName);
+      if (item) {
+        const itemWrap: itemWrap = JSON.parse(item);
+
+        return itemWrap.objectType === 'string'
+          ? itemWrap.object
+          : JSON.parse(itemWrap.object);
+      } else {
+        throw new Error(`Element by key: '${itemName}' not found!`);
+      }
+    },
+
+    set(itemName: string, item: any): void {
+      localStorage.setItem(itemName, JSON.stringify(getItemWrap(item)));
+    },
+
+    remove(itemName: string): void {
+      localStorage.removeItem(itemName);
+    },
+  };
+};
+
+const getItemWrap = (item: any): itemWrap => {
+  const itemType = typeof item;
+  item = itemType === 'object' ? JSON.stringify(item) : item;
+  const itemWrap: itemWrap = {
+    object: item,
+    objectType: itemType,
+  };
+  return itemWrap;
+};
