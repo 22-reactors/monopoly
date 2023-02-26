37db56059f53102f2b254bb84d4aba2106f1e477
Renovacio3429 <sav3429@gmail.com>
2023-02-27 00:05:25 +0700

    +

--- packages/client/src/reduxstore/monopolyStore.ts
+++ packages/client/src/reduxstore/monopolyStore.ts
@@ -8,10 +8,7 @@ export const store = configureStore({
   reducer: appReducer,
 });
 
-// Use RootState and Dispatch typing inside files that will need this reference.
-// Therefore, these definitions are created and exported within the store.ts file.
-
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch
