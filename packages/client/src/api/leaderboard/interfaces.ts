3fffe53a893ad1b1ffacf784ca0800024bf349ec
ysveshnikov <ysveshnikov@ivi.ru>
2023-02-26 21:43:35 +0700

    refs MON-42 добавляет апи

---
+++ packages/client/src/api/leaderboard/interfaces.ts
@@ -0,0 +1,15 @@
+import { IBadResponse } from '../auth/interfaces'
+import { LeaderboardResults } from '../../pages/leaderboard/leaderboard'
+
+export type Leaderboard = [
+  {
+    data: {
+      results: LeaderboardResults[],
+      reactors: ''
+    }
+  }
+];
+
+export type LeaderboardResponse = 'OK' | IBadResponse;
+export type LeaderboardGetAllResponse = Leaderboard | IBadResponse;
+
