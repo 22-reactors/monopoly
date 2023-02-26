9e52b0c2b87ef83b6c5dd6873a51391a882db937
anton.bezushko <bezushko.aiu@gmail.com>
2023-02-26 15:22:40 +0500

    Страница ошибка добавлена в роутинг

---
+++ packages/client/src/pages/index.tsx
@@ -0,0 +1,38 @@
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import ErrorPage from './Error/errorPage';
+import { Forum } from './forum/forum';
+import { Game, gameLoader } from './game/game';
+import { Home } from './home/home';
+import { Leaderboard } from './leaderboard/leaderboard';
+import Login from './login';
+import { loginLoader } from './login/login';
+import { ProfilePage, profileLoader } from './profile/profile';
+import Register from './register';
+import { registerLoader } from './register/register';
+
+const RootBoundary = () => {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <ErrorPage code={error.status} />
+    );
+  }
+
+  throw error;
+};
+
+export {
+  RootBoundary,
+  Forum,
+  Game,
+  gameLoader,
+  Home,
+  Leaderboard,
+  Login,
+  loginLoader,
+  ProfilePage,
+  profileLoader,
+  Register,
+  registerLoader,
+};
