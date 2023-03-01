46f2e7bdad0677abd2e84f649a2232f559b7c828
ysveshnikov <ysveshnikov@ivi.ru>
2023-03-01 11:57:27 +0700

    refs MON-73 дорабатывает страницу игры

--- packages/client/src/pages/game/game.tsx
+++ packages/client/src/pages/game/game.tsx
@@ -1,13 +1,17 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react'
 import { Button, ButtonVariation } from '../../components/button/button';
 import { GameEngine } from '../../game-engine/gameEngine';
 import { unAuthorizedRedirect } from '../../utils/helpers'
 import style from './game.module.scss'
+import { Link } from 'react-router-dom'
+import { links } from '../../utils/const'
+import classNames from 'classnames'
 
 export const gameLoader = unAuthorizedRedirect
 
 export const Game = () => {
   const canvasEl = useRef<HTMLCanvasElement>(null);
+  const [isShowMenu, setIsShowMenu] = useState(false);
 
   useEffect(() => { initCanvas() }, []);
 
@@ -23,12 +27,23 @@ export const Game = () => {
 
   return (
     <div className={style.gamePageWrapClass}>
-      <Button
-        className={style.reloadPageBtnClass}
-        variation={ButtonVariation.PRIMARY}
-        onClick={reloadPage}>
-        Начать сначало
-      </Button>
+      <div className={style.menuWrapper}>
+        <Button
+          className={style.btn}
+          variation={ButtonVariation.PRIMARY}
+          onClick={reloadPage}>
+          Начать сначала
+        </Button>
+        <Button
+          className={style.btn}
+          variation={ButtonVariation.PRIMARY}
+          onClick={() => {
+            setIsShowMenu((prevState) => !prevState);
+          }}>
+          {isShowMenu ? 'Закрыть меню' : 'Меню'}
+        </Button>
+        <Menu className={isShowMenu ? style.menu_show : style.menu_hide}/>
+      </div>
       <canvas
         ref={canvasEl}
         width={1000}
@@ -38,3 +53,30 @@ export const Game = () => {
     </div>
   );
 };
+
+const MENU_ITEM = [
+  {
+    title: links.profile.title,
+    to: links.profile.path
+  },
+  {
+    title: links.leaderboard.title,
+    to: links.leaderboard.path
+  },
+  {
+    title: links.forum.title,
+    to: links.forum.path
+  },
+];
+
+function Menu(props: { className: string }) {
+  return (
+    <ul className={classNames(style.menu, props.className)}>
+      {MENU_ITEM.map((item, idx) => (
+        <li className={style.menuItem} key={idx}>
+          <Link to={item.to}>{item.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
