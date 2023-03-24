7a23232ce53c5b43877b042438244efa05c90aa8
anton.bezushko <bezushko.aiu@gmail.com>
2023-03-25 01:02:19 +0500

    feature/change-sw-cache-strategy

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
         runtimeCaching: [
           {
             urlPattern: 'https://ya-praktikum.tech/api/v2/auth/user',
-            handler: 'CacheFirst',
+            handler: 'NetworkFirst',
           },
         ],
       },
