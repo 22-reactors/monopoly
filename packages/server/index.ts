92f1a07f82344f8b7791a0b57520928ff39ecf64
Renovacio3429 <sav3429@gmail.com>
2023-03-27 18:14:52 +0700

    dasdasdsa

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -11,6 +11,7 @@ const port = Number(process.env.SERVER_PORT) || 3001;
 createClientAndConnect();
 app.use(router);
 
+
 app.listen(port, () => {
   console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
 });
