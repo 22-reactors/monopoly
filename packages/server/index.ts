3bb4262136581e71292de266a4baeb322a52b328
Renovacio3429 <sav3429@gmail.com>
2023-03-27 17:47:55 +0700

    add sequelize and forum models

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,15 @@
 import dotenv from 'dotenv';
 import cors from 'cors';
 dotenv.config();
-
 import express from 'express';
-// import { createClientAndConnect } from './db';
+import { createClientAndConnect } from './db';
+import { router } from './src/router/forumRouter';
 
 const app = express();
 app.use(cors());
 const port = Number(process.env.SERVER_PORT) || 3001;
-
-// createClientAndConnect();
-
-app.get('/', (_, res) => {
-  res.json('👋 Howdy from the server :)');
-});
-
+createClientAndConnect();
+app.use(router);
 app.listen(port, () => {
   console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
 });
