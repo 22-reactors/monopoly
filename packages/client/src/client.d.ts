38b70733f8d37b4106c7d18150abe601943021fb
skaamoogs <shabalexx@yandex.ru>
2023-03-28 12:14:26 +0300

    format and remove commented code from main.tsx

--- packages/client/src/client.d.ts
+++ packages/client/src/client.d.ts
@@ -1,8 +1,6 @@
-import type { RootState } from "./reduxstore/monopolyStore";
-
+import type { RootState } from './reduxstore/monopolyStore';
 
 declare global {
-  
   const __SERVER_PORT__: number;
   interface Window {
     initialState?: RootState;
