a5ad554ce21fbcb2ccab5f9fe3015b513b3df5e6
anton.bezushko <bezushko.aiu@gmail.com>
2023-04-20 16:30:41 +0500

    Добавляет PWA

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
+import { VitePWA } from 'vite-plugin-pwa';
+
 dotenv.config();
 
 // https://vitejs.dev/config/
@@ -11,5 +13,22 @@ export default defineConfig({
   define: {
     __SERVER_PORT__: process.env.SERVER_PORT || 3001,
   },
-  plugins: [react()],
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        runtimeCaching: [
+          {
+            urlPattern: 'https://ya-praktikum.tech/api/v2/auth/user',
+            handler: 'NetworkFirst',
+          },
+        ],
+      },
+    }),
+    react(),
+  ],
 });
